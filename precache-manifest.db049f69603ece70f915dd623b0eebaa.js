ad7f0ce5af7acfa82add4efe18510cc09ba041ca
Josh Shuster <jshuster7484@gmail.com>
2020-03-30 16:57:06 -0500

    Updates

---
+++ precache-manifest.db049f69603ece70f915dd623b0eebaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4b66705683847227416ccc3890b8c4",
+    "url": "/character-sheet/index.html"
+  },
+  {
+    "revision": "88c23a7247d57460fa54",
+    "url": "/character-sheet/static/css/main.6636212f.chunk.css"
+  },
+  {
+    "revision": "ddddbab290d9d9069cd7",
+    "url": "/character-sheet/static/js/2.788e5e36.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/character-sheet/static/js/2.788e5e36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c23a7247d57460fa54",
+    "url": "/character-sheet/static/js/main.0b4bd085.chunk.js"
+  },
+  {
+    "revision": "c95c3e043bb14e344cdb",
+    "url": "/character-sheet/static/js/runtime-main.87d3fc3b.js"
+  },
+  {
+    "revision": "4da555be54f612c4e67418e366b3cc11",
+    "url": "/character-sheet/static/media/corvo.4da555be.jpg"
+  }
+]);
