09647af5edfc14b36ecb3f5574e39c2529e3defb
jshuster7484 <jshuster7484@gmail.com>
2020-03-31 22:15:02 -0500

    Added Weapon and ArmorClass components

---
+++ src/components/ArmorClass.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Stat from "./Stat";
+import { getAbilityScore, getStringNumber } from "../utils";
+
+const ArmorClass = props => {
+  const { character, inventory, modifiers } = props;
+
+  const initial = [
+    { label: "Base", value: 10 },
+    {
+      label: "Dexterity",
+      value: getAbilityScore("dex", character, modifiers),
+    },
+  ];
+
+  const items = inventory
+    .filter(item => item.armorBonus > 0)
+    .map(item => {
+      return { label: item.name, value: getStringNumber(item.armorBonus) };
+    });
+
+  const mods = initial.concat(items);
+
+  const sum = mods.reduce(function(prev, mod) {
+    return prev + parseInt(mod.value);
+  }, 0);
+
+  return <Stat label="Armor Class" modifiers={mods} value={sum} />;
+};
+
+export default ArmorClass;
---
+++ src/components/Weapon.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Stat from "./Stat";
+
+const Weapon = props => {
+  const {
+    attackBonus,
+    criticalRange,
+    damageBonus,
+    damageDie,
+    damageType,
+    name,
+  } = props;
+
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        border: "1px solid lightgray",
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <span>{name}</span>
+      <Stat label="Attack Bonus" value={attackBonus} />
+      <Stat label="Damage Die" value={damageDie} />
+      <Stat label="Critical Range" value={criticalRange} />
+    </div>
+  );
+};
+
+export default Weapon;
