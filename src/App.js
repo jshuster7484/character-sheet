754c43d7956a933d0fc924223b93dd8810bce23f
jshuster7484 <jshuster7484@gmail.com>
2020-06-02 20:38:30 -0500

    Attempt to fix deploy bug with github pages by adding basename to Router

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <Container className="App">
